fda4c606aeef2e389feec37afdd4829902ddf59c
Kapil564 <kmasih505@gmail.com>
2026-03-23 05:38:31 +0530

    remove abtract words from the vehicle class

--- Vehicle.java
+++ Vehicle.java
@@ -1,15 +1,12 @@
-public abstract class Vehicle {
+public  class Vehicle {
     private int vehicleNumber;
     private VehicleType type;
     public Vehicle (int vehicleNumber, VehicleType vehicleType){
         this.vehicleNumber = vehicleNumber;
         this.type = vehicleType;
     }
-    public Vehicle getDetails() {
-        return this;
-    }
+
     public VehicleType getType() {
         return type;
     }
-    public abstract boolean canFitInSlot(SlotType slotType);
 }
